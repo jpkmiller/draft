cdef91fbaa7181a8557b8d0e1fe493ca5d02585b
jpkmiller <josef.mueller@vector.com>
2022-04-27 12:07:02 +0200

    impl3a: renamed service and started simple.

---
+++ impl3a-manager-service/src/main/java/com/example/EPP.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class EPP {
+    final String name;
+    final String location;
+
+    public EPP(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+}
---
+++ impl3a-manager-service/src/main/java/com/example/Impl3Resource.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import javax.ws.rs.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
+
+
+@Path("/v3")
+public class Impl3Resource {
+
+    Map<String, String> epps = new HashMap<>();
+
+    @POST
+    @Path("/register")
+    @Consumes(APPLICATION_JSON)
+    @Produces(TEXT_PLAIN)
+    public String registerEPP(EPP epp) {
+        this.epps.put(epp.name, epp.location);
+        return "registered " + epp.name;
+    }
+
+    @GET
+    @Produces(APPLICATION_JSON)
+    public List<String> getEPPs() {
+        return epps.entrySet().stream().map(Objects::toString).collect(Collectors.toList());
+    }
+}
